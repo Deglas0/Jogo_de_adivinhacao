624ac92fb5bb5a48cca4aa1ac9149b006b721fda
Douglas miguel da silva <124850234+Deglas0@users.noreply.github.com>
2023-05-07 03:26:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jogo_de_adivinhacao
+ Descubra o numero que o programa gerou
---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Bem vindo ao Jogo de advinhação
+                O programa irá gerar um numero aleatóriamente
+                E
+                Você deverá adivinhar um numero inteiro entre 1 a 100
+                em 3 tentativas
+                                
+                Boa sorte ;/
+                """);
+        int tentativas = 0;
+        int numeroAleatorio = new Random().nextInt(100);
+        int n = 0;
+        String maiorQ;
+        String menorQ;
+        maiorQ = "Esse numero é maior que o numero gerado...";
+        menorQ = "Esse numero é menor que o numero gerado...";
+
+        System.out.println(numeroAleatorio);
+
+        while (tentativas < 3) {
+            Scanner numeroDoUsuario = new Scanner(System.in);
+            n = numeroDoUsuario.nextInt();
+            if (n == numeroAleatorio) {
+                System.out.println("Parabéns vc acertou");
+            }
+            if (tentativas == 3) {
+                System.out.println("Suas chances acabaram");
+            } else {
+                if (n > numeroAleatorio) {
+                    System.out.println(maiorQ);
+                }
+                if (n < numeroAleatorio) {
+                    System.out.println(menorQ);
+                }
+
+                tentativas++;
+                System.out.println("tentativas: "+tentativas);
+            }
+
+
+        }
+    }
+}
